f8bd860439d2eb0adb736bc00d7d9ee3010d03b8
Astecom <astecom@mail.ru>
2020-12-09 13:27:26 +0400

    Build script added

--- build.bat
+++ build.bat
@@ -1,17 +1,5 @@
 @echo off
 
-echo "Building jsdoc..."
-
-echo | jsdoc poonya.js
-
-echo "Removing old docs...";
-
-rmdir /q /s "docs"
-
-echo "Move docs for docs folder..."
-
-ren "out" "docs"
-
 node build/build-default.js
 
 pause
--- build/build-default.js
+++ build/build-default.js
@@ -1,17 +1,68 @@
 const { execSync } = require("child_process")
+    , { copyFileSync, renameSync, rmdirSync } = require('fs')
     , readline = require("readline");
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+let question, allow = ["y", "yes", "н", "нуы"];
 
-rl.question("Commit changes? (Y/N)", (commit) => {
-    if(["y", "yes", "н", "нуы"].includes(commit.toLowerCase())) {
-        rl.question("Commit message: ", (message) => {
-            execSync('git add -A --');
-            execSync('git commit --quiet -m"' + message + '"');
-            execSync('git push');
+{
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    question = (message) => {
+        return new Promise((res, rej) => {
+            rl.question(message, (responce) => {
+                res(responce)
+            });
         })
     }
-});
+}
+
+async function main(){
+    console.log("building jsdoc");
+
+    execSync('jsdoc poonya.js');
+
+    rmdirSync('docs', { recursive: true });
+
+    renameSync('out', 'docs');
+
+    try {
+        const old_conf = require(__dirname + '/build-data/package.json')
+            , new_conf = require(__dirname + '/../package.json');
+
+        if(old_conf.version != new_conf.version) {
+            if(!allow.includes((await question('The version has been changed, have you updated the images, and the readme? (Y/N) ')).toLowerCase()))
+                process.exit(0);
+        }
+    } catch (e) {
+        console.log('Old or new config file not found, no changes')
+    }
+
+    try {
+        copyFileSync('package.json', 'build/build-data/package.json');
+    } catch (e) {
+        console.log('Apparently you deleted the package.json file')
+    }
+
+    const commit = await question("Commit changes? (Y/N) ");
+
+    if(allow.includes(commit.toLowerCase())){
+        const message = await question('Commit message: ');
+
+        execSync('git add -A --');
+        execSync('git commit --quiet -m"' + message + '"');
+
+        const push = await question('Push changes? (Y/N) ');
+
+        if(allow.includes(push.toLowerCase()))
+            execSync('git push');
+
+        process.exit(0);
+    } else {
+        process.exit(0);
+    }
+}
+
+main();
